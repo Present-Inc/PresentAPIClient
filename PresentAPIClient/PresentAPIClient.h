440baa82c996cc949402a27c9292fd4cd45ae2ad
justinmakaila <justinmakaila@gmail.com>
2014-08-13 17:53:01 -0400

    Initial Commit

---
+++ PresentAPIClient/PresentAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  PresentAPIClient.h
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 8/13/14.
+//  Copyright (c) 2014 present. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PresentAPIClient.
+FOUNDATION_EXPORT double PresentAPIClientVersionNumber;
+
+//! Project version string for PresentAPIClient.
+FOUNDATION_EXPORT const unsigned char PresentAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PresentAPIClient/PublicHeader.h>
+
+
