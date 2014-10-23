87744de6807ef73b2c989d67b4e08116a4ebbf47
Justin Makaila <justinmakaila@gmail.com>
2014-10-23 14:18:15 -0400

    Made struct internal

--- PresentAPIClient/PresentAPIClient.swift
+++ PresentAPIClient/PresentAPIClient.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftyJSON
 
-public struct APIEnvironment {
+internal struct APIEnvironment {
     static let Version = "2014-09-09"
     static let PresentVersionHeader = "Present-Version"
     static let SessionTokenHeader = "Present-User-Context-Session-Token"
