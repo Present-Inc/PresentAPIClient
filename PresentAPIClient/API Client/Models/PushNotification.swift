559b64b3fa681bae3c8e0d3ad8e2854a7556d17d
Justin Makaila <justinmakaila@gmail.com>
2014-11-03 18:43:14 -0500

    Added model to represent a push notification from the Present API

---
+++ PresentAPIClient/API Client/Models/PushNotification.swift
@@ -0,0 +1,33 @@
+//
+//  PushNotification.swift
+//  PresentAPIClient
+//
+//  Created by Justin Makaila on 11/3/14.
+//  Copyright (c) 2014 present. All rights reserved.
+//
+
+import Foundation
+
+public class PushNotification: NSObject {
+    public let alert: String?
+    public let contentAvailable: Int?
+    public let badge: Int?
+    public let activityId: String?
+    public let type: ActivityType?
+    
+    init(dictionary: [NSObject: AnyObject]) {
+        if let aps = dictionary["aps"] as? [NSObject: AnyObject] {
+            alert = aps["alert"] as? String
+            contentAvailable = aps["contentAvailable"] as? Int
+            badge = aps["badge"] as? Int
+        }
+        
+        activityId = dictionary["_id"] as? String
+        
+        if let rawType = dictionary["type"] as? String {
+            type = ActivityType(rawValue: rawType)
+        } else {
+            type = .Invalid
+        }
+    }
+}
