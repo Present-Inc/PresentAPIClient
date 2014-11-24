ec4572b0dce3621d3df62070683adc6c0193fff7
Justin Makaila <justinmakaila@gmail.com>
2014-11-24 14:24:34 -0500

    Added PushNotificationPlatform to the APIEnvironment struct

--- PresentAPIClient/PresentAPIClient.swift
+++ PresentAPIClient/PresentAPIClient.swift
@@ -9,11 +9,19 @@
 import Foundation
 
 internal struct APIEnvironment {
+    // API version to use
     static let Version = "2014-09-09"
+    
+    // Version header key
     static let PresentVersionHeader = "Present-Version"
+    
+    // Session token header key
     static let SessionTokenHeader = "Present-User-Context-Session-Token"
+    
+    // User id header key
     static let UserIdHeader = "Present-User-Context-User-Id"
     
+    // The base URL for all request
     static let baseUrl: NSURL = {
         var webProtocol: String = "https"
         var subdomain: String
@@ -29,5 +37,16 @@ internal struct APIEnvironment {
         #endif
         
         return NSURL(string: "\(webProtocol)://\(subdomain).present.tv/\(version)/")!
+    }()
+    
+    // The push notification platform for the client
+    static let PushNotificationPlatform: String = {
+        #if DEVELOPMENT
+            return "APNS_SANDBOX"
+        #elseif STAGING
+            return"APNS_SANDBOX"
+        #else
+            return "APNS"
+        #endif
     }()
 }
